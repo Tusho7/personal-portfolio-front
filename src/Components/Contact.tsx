23f41ec28900ecb0fe5baaf8780e0e9dbf0f1213
Tusho7 <sandrotushurashvili7@gmail.com>
2023-06-01 23:13:52 +0400

    style: Change some contact styles

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,6 +5,7 @@ import CodeLogo from "../assets/code-logo.svg";
 import { CodeLogoContainer } from "../styles/Contact-styles/CodeLogoContainer";
 import { CodeImg } from "../styles/Home-Styles/CodeImg";
 import { MediaTitle } from "../styles/Contact-styles/MediaTitle";
+import { PhoneCall } from "../styles/Contact-styles/PhoneCall";
 
 function Contact() {
   return (
@@ -13,7 +14,10 @@ function Contact() {
 
         <p>What would you do if you had a software expert available at your fingertips?</p>
         <p>Want to start new project? Contact me on <a href='https://www.linkedin.com/in/sandro-tushurashvili/' target='_blank'>Linkedin</a>.</p>
-        <MediaTitle>sandrotushurashvili7@gmail.com</MediaTitle>
+        <MediaTitle>
+          <a href="mailto:sandrotushurashvili7@gmail.com">
+          sandrotushurashvili7@gmail.com</a></MediaTitle>
+          <p>Call me at <a href="tel:555 05 00 01"> 555 05 00 01</a></p>
         <p>You can also follow me on <a href='https://www.instagram.com/sandrotushurashvili/' target='_blank'>Intagram</a>.</p>
         </ContactTitlesContainer>
 
@@ -21,7 +25,7 @@ function Contact() {
             <CodeImg src={CodeLogo} />
         </CodeLogoContainer>
 
-        <h1>sandrotushurashvili7@gmail.com</h1>
+        <h1><a href="mailto:sandrotushurashvili7@gmail.com">sandrotushurashvili7@gmail.com</a></h1>
         <MyGlobe/>
     </ContactContainer>
   )
